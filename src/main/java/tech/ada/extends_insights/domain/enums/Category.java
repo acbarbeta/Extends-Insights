9022f93032a51a82864e66edcee7449a2b91fbcd
Ana Clara <aclara.bvicente@gmail.com>
2024-02-16 22:07:54 -0300

    Populates category enum

--- src/main/java/tech/ada/extends_insights/domain/enums/Category.java
+++ src/main/java/tech/ada/extends_insights/domain/enums/Category.java
@@ -2,5 +2,11 @@ package tech.ada.extends_insights.domain.enums;
 
 public enum Category {
     SOFTWARE_DEVELOPMENT,
+    INFRASTRUCTURE_AND_NETWORKS,
+    IA_AND_MACHINE_LEARNING,
+    CYBERSECURITY,
+    DATA_SCIENCE_AND_BIG_DATA,
+    WEB_TECHNOLOGIES,
+    OS
 
 }
