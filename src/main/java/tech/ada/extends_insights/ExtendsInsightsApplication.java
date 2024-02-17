d00ba01edb26f70a05025e908891b9139b17b1b9
Ana Clara <aclara.bvicente@gmail.com>
2024-02-16 21:10:23 -0300

    Initial setup

---
+++ src/main/java/tech/ada/extends_insights/ExtendsInsightsApplication.java
@@ -0,0 +1,13 @@
+package tech.ada.extends_insights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendsInsightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtendsInsightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/ada/extends_insights/ExtendsInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.extends_insights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtendsInsightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
