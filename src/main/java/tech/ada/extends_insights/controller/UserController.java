9de4d72e25441af6f2a91bcaf8303e5249bdfa7f
Elisa Souza <contato.elisasouza@hotmail.com>
2024-02-17 21:01:40 -0300

    Created endpoint GET byUsername for User

--- src/main/java/tech/ada/extends_insights/controller/UserController.java
+++ src/main/java/tech/ada/extends_insights/controller/UserController.java
@@ -4,9 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.ada.extends_insights.domain.entities.User;
 import tech.ada.extends_insights.domain.models.requests.UserRequest;
 import tech.ada.extends_insights.repository.UserRepository;
@@ -29,5 +27,15 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
-}
 
+    @GetMapping("/users/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+
+        if(user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
+    }
+}
