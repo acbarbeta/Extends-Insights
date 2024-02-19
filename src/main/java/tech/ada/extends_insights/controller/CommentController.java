4159234a9c7ff9339f51e99f0bb3184a0cdbdb62
Ariane Padilha <ariane.padilha@outlook.com.br>
2024-02-18 21:18:20 -0300

    Creates Comment put endpoint

--- src/main/java/tech/ada/extends_insights/controller/CommentController.java
+++ src/main/java/tech/ada/extends_insights/controller/CommentController.java
@@ -9,6 +9,8 @@ import tech.ada.extends_insights.domain.entities.Comment;
 import tech.ada.extends_insights.domain.models.requests.CommentRequest;
 import tech.ada.extends_insights.repository.CommentRepository;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
@@ -26,6 +28,20 @@ public class CommentController {
         Comment convertedcomment = modelMapper.map(commentRequest, Comment.class);
         Comment newComment = commentRepository.save(convertedcomment);
         return ResponseEntity.status(HttpStatus.CREATED).body(newComment);
+    }
+
+    @PutMapping("/comments/{id}")
+    public ResponseEntity<Comment> updateComment(@PathVariable Long id, @RequestBody CommentRequest commentRequest) {
+        Optional<Comment> optionalComment = commentRepository.findById(id);
+        if (optionalComment.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Comment existingComment = optionalComment.get();
+        modelMapper.map(commentRequest, existingComment);
+
+        Comment updatedComment = commentRepository.save(existingComment);
+        return ResponseEntity.ok(updatedComment);
     }
 
 }
