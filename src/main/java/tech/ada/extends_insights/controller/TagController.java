5eb0b778a44bd0c7b4e3f66df34e1c897139a640
Ana Clara <aclara.bvicente@gmail.com>
2024-02-21 21:36:20 -0300

    Creates TagController file and creates the endpoint POST to tags

---
+++ src/main/java/tech/ada/extends_insights/controller/TagController.java
@@ -0,0 +1,34 @@
+package tech.ada.extends_insights.controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import tech.ada.extends_insights.domain.entities.Tag;
+import tech.ada.extends_insights.domain.models.requests.TagRequest;
+import tech.ada.extends_insights.repository.TagRepository;
+
+@RestController("/tags")
+public class TagController {
+    private final TagRepository tagRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public TagController(TagRepository tagRepository, ModelMapper modelMapper) {
+        this.tagRepository = tagRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping("/tags-creation")
+    public ResponseEntity<Tag> createTag(@RequestBody TagRequest request) {
+
+        Tag convertedTag = modelMapper.map(request, Tag.class);
+
+        Tag newTag = tagRepository.save(convertedTag);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTag);
+    }
+}
