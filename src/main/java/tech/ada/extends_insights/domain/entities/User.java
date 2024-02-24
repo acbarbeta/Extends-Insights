b722350338b88e933ba06365073ca407718b4846
Elisa Souza <contato.elisasouza@hotmail.com>
2024-02-24 17:46:45 -0300

    Adding validation dependencies in pom.xml and adding annotations to the User entity

--- src/main/java/tech/ada/extends_insights/domain/entities/User.java
+++ src/main/java/tech/ada/extends_insights/domain/entities/User.java
@@ -1,9 +1,13 @@
 package tech.ada.extends_insights.domain.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,10 +21,17 @@ public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long userId;
+  @NotBlank(message = "This attribute is requeried!")
+  @Size(min = 4, max = 20,message = "This attribute must contain at least 4 characters and a maximum of 20")
   private String username;
+  @NotBlank(message = "This attribute is requeried!")
+  @Size(min = 8, message = "This attribute must contain at least 8 characters")
   private String password;
+  @Email
+  @NotBlank(message = "This attribute requires a valid email")
   private String email;
   private LocalDateTime createdOn;
+  @UpdateTimestamp
   private LocalDateTime updatedOn;
   @OneToMany(mappedBy = "author")
   private List<Publication> publications;
