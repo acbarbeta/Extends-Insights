84fed27f5910fde9bd4dbe19da97ab04c24e6b85
Ana Clara <aclara.bvicente@gmail.com>
2024-02-25 21:04:31 -0300

    Changes requests to use Lombok

--- src/main/java/tech/ada/extends_insights/domain/models/requests/PublicationRequest.java
+++ src/main/java/tech/ada/extends_insights/domain/models/requests/PublicationRequest.java
@@ -1,24 +1,14 @@
 package tech.ada.extends_insights.domain.models.requests;
 
+import lombok.Getter;
 import tech.ada.extends_insights.domain.entities.User;
 import tech.ada.extends_insights.domain.enums.Category;
 import tech.ada.extends_insights.domain.entities.Tag;
-
+@Getter
 public class PublicationRequest {
     private String title;
     private String content;
     private User user;
     private Category category;
     private Tag tag;
-
-    public PublicationRequest() {
-    }
-
-    public PublicationRequest(String title, String content, User user, Category category, Tag tag) {
-        this.title = title;
-        this.content = content;
-        this.user = user;
-        this.category = category;
-        this.tag = tag;
-    }
 }
--- src/main/java/tech/ada/extends_insights/domain/models/requests/TagRequest.java
+++ src/main/java/tech/ada/extends_insights/domain/models/requests/TagRequest.java
@@ -1,17 +1,8 @@
 package tech.ada.extends_insights.domain.models.requests;
 
-import tech.ada.extends_insights.domain.entities.Publication;
+import lombok.Getter;
 
+@Getter
 public class TagRequest {
     private String title;
-    private Publication publication;
-
-    public TagRequest() {
-
-    }
-
-    public TagRequest(String title, Publication publication) {
-        this.title = title;
-        this.publication = publication;
-    }
 }
