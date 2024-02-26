b1778059b515a0aed7d0b666bd5be8d77914ccf1
Ana Clara <aclara.bvicente@gmail.com>
2024-02-25 21:06:21 -0300

    Changes Tag to List of tags in request

--- src/main/java/tech/ada/extends_insights/domain/models/requests/PublicationRequest.java
+++ src/main/java/tech/ada/extends_insights/domain/models/requests/PublicationRequest.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 import tech.ada.extends_insights.domain.entities.User;
 import tech.ada.extends_insights.domain.enums.Category;
 import tech.ada.extends_insights.domain.entities.Tag;
+
+import java.util.List;
+
 @Getter
 public class PublicationRequest {
     private String title;
     private String content;
     private User user;
     private Category category;
-    private Tag tag;
+    private List<Tag> tag;
 }
