e92e62223a335072d6c8b78ec3446b25c2fc1093
MaynaraNachbar-Menlo <maynara.nachbar@menlopagamentos.com>
2024-03-11 18:32:17 -0300

    Adds PublicationServiceImpl and Adjust PublicationController

--- src/main/java/tech/ada/extends_insights/service/PublicationService.java
+++ src/main/java/tech/ada/extends_insights/service/PublicationService.java
@@ -1,4 +1,22 @@
 package tech.ada.extends_insights.service;
 
+import tech.ada.extends_insights.domain.entities.Publication;
+import tech.ada.extends_insights.domain.entities.Tag;
+import tech.ada.extends_insights.domain.entities.User;
+import tech.ada.extends_insights.domain.enums.Category;
+import tech.ada.extends_insights.domain.models.requests.PublicationRequest;
+import tech.ada.extends_insights.domain.models.requests.UpdatePublicationRequest;
+
+import java.util.List;
+
 public interface PublicationService {
+    Publication createPublication(PublicationRequest request);
+    List<Publication> readAllPublications();
+    Publication readPublicationById(Long id);
+    List<Publication> readPublicationByTitle(String title);
+    List<Publication> getPublicationByCategory(Category category);
+    List<Publication> getPublicationByTag(Tag tag);
+    List<Publication> getPublicationByUser(User author);
+    Publication updatePublication(Long id, UpdatePublicationRequest updatePublicationRequest);
+    void deletePublication(Long id);
 }
--- src/main/java/tech/ada/extends_insights/service/impl/PublicationServiceImpl.java
+++ src/main/java/tech/ada/extends_insights/service/impl/PublicationServiceImpl.java
@@ -1,4 +1,114 @@
 package tech.ada.extends_insights.service.impl;
 
-public class PublicationServiceImpl {
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import tech.ada.extends_insights.domain.entities.Publication;
+import tech.ada.extends_insights.domain.entities.Tag;
+import tech.ada.extends_insights.domain.entities.User;
+import tech.ada.extends_insights.domain.enums.Category;
+import tech.ada.extends_insights.domain.models.requests.PublicationRequest;
+import tech.ada.extends_insights.domain.models.requests.UpdatePublicationRequest;
+import tech.ada.extends_insights.repository.PublicationRepository;
+import tech.ada.extends_insights.service.PublicationService;
+import tech.ada.extends_insights.service.exceptions.ObjectNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PublicationServiceImpl implements PublicationService {
+
+    private final PublicationRepository publicationRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public PublicationServiceImpl(PublicationRepository publicationRepository, ModelMapper modelMapper) {
+        this.publicationRepository = publicationRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public Publication createPublication(PublicationRequest request) {
+        Publication convertedPublication = modelMapper.map(request, Publication.class);
+        return publicationRepository.save(convertedPublication);
+    }
+
+    @Override
+    public List<Publication> readAllPublications() {
+        Optional<List<Publication>> optionalPublications = Optional.ofNullable(publicationRepository.findAll());
+        return optionalPublications.orElseThrow(() -> new ObjectNotFoundException("Publications not found"));
+    }
+
+    @Override
+    public Publication readPublicationById(Long id) {
+        Optional<Publication> optionalPublication = publicationRepository.findById(id);
+        return optionalPublication.orElseThrow(() -> new ObjectNotFoundException("Publication not found"));
+    }
+
+    @Override
+    public List<Publication> readPublicationByTitle(String title) {
+        var publicationByTitle = publicationRepository.findByPublicationTitle(title);
+        if(publicationByTitle == null) {
+            return null;
+        }
+        return publicationByTitle;
+    }
+
+    @Override
+    public List<Publication> getPublicationByCategory(Category category) {
+        var publicationByCategory = publicationRepository.findByCategory(category);
+        if(publicationByCategory == null) {
+            return null;
+        }
+        return publicationByCategory;
+    }
+
+    @Override
+    public List<Publication> getPublicationByTag(Tag tag) {
+        var publicationByTag = publicationRepository.findByTags(tag);
+        if(publicationByTag == null) {
+            return null;
+        }
+        return publicationByTag;
+    }
+
+    @Override
+    public List<Publication> getPublicationByUser(User author) {
+        var publicationByUser = publicationRepository.findByAuthor(author);
+        if(publicationByUser == null) {
+            return null;
+        }
+        return publicationByUser;
+    }
+
+    @Override
+    public Publication updatePublication(Long id, UpdatePublicationRequest updatePublicationRequest) {
+        Optional<Publication> optionalPublication = publicationRepository.findById(id);
+
+        if (optionalPublication.isPresent()) {
+            Publication publication = optionalPublication.get();
+            publication.setPublicationTitle(updatePublicationRequest.getTitle());
+            publication.setPublicationBody(updatePublicationRequest.getContent());
+            publication.setCategory(updatePublicationRequest.getCategory());
+            publication.setTags(updatePublicationRequest.getTags());
+            publicationRepository.save(publication);
+            return publication;
+        }
+        return null;
+    }
+
+    @Override
+    public void deletePublication(Long id) {
+        Optional<Publication> optionalPublication = publicationRepository.findById(id);
+
+        if (optionalPublication.isPresent()) {
+            publicationRepository.deleteById(id);
+            ResponseEntity.noContent().build();
+        } else {
+            ResponseEntity.notFound().build();
+        }
+    }
 }
